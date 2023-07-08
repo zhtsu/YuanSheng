da4279c8ae3dc1be6b6fe957b6705bd97837154a
zhtsu <zhtsu@qq.com>
2023-07-08 23:39:44 +0800

    Complete down key

--- Game.gd
+++ Game.gd
@@ -1,5 +1,8 @@
 extends Control
 
+signal game_over
+signal game_start
+
 const KEY_SCENE = preload("res://Key.tscn")
 const NEXT_PROGRESS = {
 	128: 258,
@@ -9,12 +12,56 @@ const NEXT_PROGRESS = {
 	648: 0,
 	0: 128
 }
+const MAX_WORD_COUNT = 134
+
+var PATH_DICT
 
 var key_speed = 10
 var words_dict = {}
-var next_created_key_number = 0
+var next_created_follow_number = 0
+var current_follow_queue = []
+# A follow-path for every follow
+var current_follow_path_queue = []
 
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	PATH_DICT = {
+		"D": $Container/Center/Keyboard/PathD,
+		"F": $Container/Center/Keyboard/PathF,
+		"J": $Container/Center/Keyboard/PathJ,
+		"K": $Container/Center/Keyboard/PathK
+	}
+	
+	var words_file = FileAccess.open("res://Assets/words.txt", FileAccess.READ)
+	var words = words_file.get_as_text()
+	for i in range(words.length()):
+		words_dict[i] = words[i]
+	words_file.close()
+	
+	var path_key_pool = _gen_path_key_pool()
+	for i in range(5):
+		if path_key_pool.is_empty():
+			path_key_pool = _gen_path_key_pool()
+		var path_key = path_key_pool.pick_random()
+		var path = PATH_DICT.get(path_key)
+		var word_number = next_created_follow_number
+		var progress = 648 - (word_number * 130)
+		var new_follow = _create_key(path, words_dict.get(word_number), progress)
+		current_follow_queue.append(new_follow)
+		current_follow_path_queue.append(path_key)
+		path_key_pool.erase(path_key)
+		next_created_follow_number += 1
+	
+	$AnimationPlayer.play("Begin")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# Create a new follow to include a new key scene, and return the new follow
 func _create_key(path:Path2D, key_text:String, progress:int = 0):
 	var new_key = KEY_SCENE.instantiate()
 	new_key.call("set_label_text", key_text)
@@ -24,72 +71,80 @@ func _create_key(path:Path2D, key_text:String, progress:int = 0):
 	follow.progress = progress
 	follow.add_child(new_key)
 	path.add_child(follow)
+	return follow
 
 
 func _down_keys():
-	for follow in $Container/Center/Keyboard/PathA.get_children():
-		follow.progress = NEXT_PROGRESS.get(int(follow.progress))
-	for follow in $Container/Center/Keyboard/PathB.get_children():
-		follow.progress = NEXT_PROGRESS.get(int(follow.progress))
-	for follow in $Container/Center/Keyboard/PathC.get_children():
-		follow.progress = NEXT_PROGRESS.get(int(follow.progress))
+	_destroy_bottom_key()
+	if next_created_follow_number < MAX_WORD_COUNT:
+		_create_next_follow()
+	else:
+		emit_signal("game_over")
+	
 	for follow in $Container/Center/Keyboard/PathD.get_children():
 		follow.progress = NEXT_PROGRESS.get(int(follow.progress))
+	for follow in $Container/Center/Keyboard/PathF.get_children():
+		follow.progress = NEXT_PROGRESS.get(int(follow.progress))
+	for follow in $Container/Center/Keyboard/PathJ.get_children():
+		follow.progress = NEXT_PROGRESS.get(int(follow.progress))
+	for follow in $Container/Center/Keyboard/PathK.get_children():
+		follow.progress = NEXT_PROGRESS.get(int(follow.progress))
 
 
-func _gen_path_pool():
-	var follow_pool = [
-		$Container/Center/Keyboard/PathA,
-		$Container/Center/Keyboard/PathB,
-		$Container/Center/Keyboard/PathC,
-		$Container/Center/Keyboard/PathD
-	]
-	return follow_pool
+func _gen_path_key_pool():
+	var path_key_pool = ["D", "F", "J", "K"]
+	return path_key_pool
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var words_file = FileAccess.open("res://Assets/words.txt", FileAccess.READ)
-	var words = words_file.get_as_text()
-	for i in range(words.length()):
-		words_dict[i] = words[i]
-	words_file.close()
-	
-	var path_pool = _gen_path_pool()
-	for i in range(5):
-		if path_pool.is_empty():
-			path_pool = _gen_path_pool()
-		var path = path_pool.pick_random()
-		var word_number = next_created_key_number
-		var progress = 648 - (word_number * 130)
-		print(progress)
-		_create_key(path, words_dict.get(word_number), progress)
-		path_pool.erase(path)
-		next_created_key_number += 1
+
+func _is_correct_keydown(path_key:String):
+	if not current_follow_path_queue.is_empty():
+		return current_follow_path_queue.front() == path_key
+	return false
+
+
+func _destroy_bottom_key():
+	if not current_follow_queue.is_empty():
+		var deleted_follow = current_follow_queue.front()
+		current_follow_queue.erase(current_follow_queue.front())
+		deleted_follow.queue_free()
 		
-	$AnimationPlayer.play("Begin")
-	
+	if not current_follow_path_queue.is_empty():
+		current_follow_path_queue.erase(current_follow_path_queue.front())
 
 
-func _create_next_key():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _create_next_follow():
+	var path_key_pool = _gen_path_key_pool()
+	var path_key = path_key_pool.pick_random()
+	var word = words_dict.get(next_created_follow_number)
+	var new_follow = _create_key(PATH_DICT.get(path_key), word)
+	current_follow_queue.append(new_follow)
+	current_follow_path_queue.append(path_key)
+	next_created_follow_number += 1
 
 
 func _on_d_button_pressed():
+	if _is_correct_keydown("D"):
+		pass
 	_down_keys()
 
 
 func _on_f_button_pressed():
-	pass # Replace with function body.
+	if _is_correct_keydown("F"):
+		pass
+	_down_keys()
 
 
 func _on_j_button_pressed():
-	pass # Replace with function body.
+	if _is_correct_keydown("J"):
+		pass
+	_down_keys()
 
 
 func _on_k_button_pressed():
-	pass # Replace with function body.
+	if _is_correct_keydown("K"):
+		pass
+	_down_keys()
+
+
+func _game_over():
+	pass
